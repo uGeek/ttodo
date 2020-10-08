6aaee924b3801efd2b3210d3222246119a164ad8
uGeek <ugeekpodcast@gmail.com>
2020-10-08 20:31:06 +0200

    Update taskell.sh

--- taskell.sh
+++ taskell.sh
@@ -10,30 +10,29 @@
 #
 
 grep -v "$(cat $1 | sed -n 1p)" todo.txt | grep -v "$(cat $1 | sed -n 2p)" | grep -v "$(cat $1 | sed -n 3p)" > todot.txt 
-grep "$(cat $1 | sed -n 1p)" todo.txt | grep "$(cat $1 | sed -n 2p)" | grep "$(cat $1 | sed -n 3p)" > todor.txt
+grep "$(cat $1 | sed -n 1p)" todo.txt >  todoe.txt
+grep "$(cat $1 | sed -n 2p)" todo.txt >> todoe.txt
+grep "$(cat $1 | sed -n 3p)" todo.txt >> todoe.txt
 
 echo "## $(cat $1 | sed -n 1p)" > $1.md
 echo "" >> $1.md 
-grep $(cat $1 | sed -n 1p) todot.txt  | sed 's/^/- /' >> $1.md
+grep $(cat $1 | sed -n 1p) todoe.txt  | sed 's/^/- /' >> $1.md
 echo "" >> $1.md
 
 echo "## $(cat $1 | sed -n 2p)" >> $1.md
 echo "" >> $1.md
-grep $(cat $1 | sed -n 2p) todot.txt  | sed 's/^/- /' >> $1.md
+grep $(cat $1 | sed -n 2p) todoe.txt  | sed 's/^/- /' >> $1.md
 echo "" >> $1.md
 
 echo "" >> $1.md
 echo "## $(cat $1 | sed -n 3p)" >> $1.md
 echo "" >> $1.md
-grep $(cat $1 | sed -n 3p) todot.txt  | sed 's/^/- /' >> $1.md
+grep $(cat $1 | sed -n 3p) todoe.txt  | sed 's/^/- /' >> $1.md
 echo "" >> $1.md
 
 
 taskell $1.md
 
 
-cat $1.md | sed s'|- ||'g | grep -v "^##" | grep . > todo1.txt
-cat todor.txt >> todo1.txt
-
-
-
+cat todot.txt > todo.txt
+cat $1.md | sed s'|- ||'g | grep -v "^##" | grep . >> todo.txt
